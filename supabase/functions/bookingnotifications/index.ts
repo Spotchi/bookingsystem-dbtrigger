7d8598de74d6206ceb211a40c20353c4fdf070a6
friedger <mail@friedger.de>
2025-07-04 17:59:22 +0200

    chore: move bookingnotifications function to supabase

--- supabase/functions/bookingnotifications/index.ts
+++ supabase/functions/bookingnotifications/index.ts
@@ -1,10 +1,10 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+
 // Setup type definitions for built-in Supabase Runtime APIs
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
-import { SMTPClient } from "https://deno.land/x/denomailer/mod.ts";
-import { getTemplateFromLanguageCode } from "./language-utils.ts";
-import { sendEmail } from "./sendEmail.ts";
-import { handleCalenderEntry } from "./handleCalendar.ts";
-serve(async (req) => {
+import { handleCalenderEntry } from "./../../../src/handleCalendar.ts";
+import { sendEmail } from "./../../../src/sendEmail.ts";
+
+Deno.serve(async (req) => {
   try {
     const isAuthenticated =
       req.headers.get("x-supabase-webhook-source") ===
@@ -94,3 +94,15 @@ serve(async (req) => {
     }
   }
 });
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/bookingnotifications' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
